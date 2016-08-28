cdce4de002ddc36d275ebff020e6ce9d373bb25e
Yordan Miladinov <yordan@4web.bg>
2016-08-28 12:00:26 +0300

    test sphere and ray intersections

--- tests/primitive.rs
+++ tests/primitive.rs
@@ -12,16 +12,37 @@ use rayrust::ray::{ self, Intersectable };
 #[test]
 fn test_sphere_intersection() {
     let sphere = primitive::Sphere::new(
-        &na::Vector3::new(0.0, 0.0, 0.0), // center
+        &na::Vector3::new(0.0, 0.0, 5.0), // center
         1.0                               // radius
     );
-    let ray = ray::Ray::<f32>::new(
+
+    // Two intersections
+    let ray2 = ray::Ray::<f32>::new(
         &na::Vector3::new(0.0, 0.0,  5.0),
         &na::Vector3::new(0.0, 0.0, -1.0)
     );
+    let ts2 = sphere.intersections(&ray2);
+    assert_eq!(ts2.len(), 2);
+    assert_approx_eq!(ts2[0],  9.0);
+    assert_approx_eq!(ts2[1], 11.0);
+    assert_eq!(ts2[0], sphere.intersection(&ray2).unwrap());
 
-    let ts = sphere.intersections(&ray);
-    assert_approx_eq!(ts[0], 4.0);
-    assert_approx_eq!(ts[1], 6.0);
+    // One intersection
+    let ray1 = ray::Ray::<f32>::new(
+        &na::Vector3::new(0.5, 0.0,  5.0),
+        &na::Vector3::new(0.0, 0.0, -1.0)
+    );
+    let ts1 = sphere.intersections(&ray1);
+    assert_eq!(ts1.len(), 1);
+    assert_approx_eq!(ts1[0], 10.0);
+    assert_eq!(ts1[0], sphere.intersection(&ray1).unwrap());
+
+    // Zero intersections
+    let ray0 = ray::Ray::<f32>::new(
+        &na::Vector3::new(0.51, 0.0,  5.0),
+        &na::Vector3::new(0.0, 0.0, -1.0)
+    );
+    let ts0 = sphere.intersections(&ray0);
+    assert_eq!(ts0.len(), 0);
+    assert!(sphere.intersection(&ray0).is_none());
 }
-
