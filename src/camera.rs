6a1858cc68804cf4e98f81466b8dbd4241d505ed
Yordan Miladinov <yordan@4web.bg>
2016-08-28 14:07:31 +0300

    stuff

--- src/camera.rs
+++ src/camera.rs
@@ -7,6 +7,15 @@ struct Camera {
 
 impl Camera {
     pub fn new() -> Camera {
-        
+        Camera {
+        }
     }
 }
+
+pub fn raster_to_ndc(w: u16, h: u16, p: &na::Vector3<u16>) -> na::Vector3<f32> {
+    let x = (p.x as f32) / (w as f32);
+    let y = (p.y as f32) / (h as f32);
+    na::Vector3::new(x, y, 0.0)
+}
+
+// pub fn ndc_to_
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod camera;
 pub mod color;
 pub mod image;
 pub mod primitive;
